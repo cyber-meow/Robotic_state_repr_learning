1571797fc67a4bebe920dd2a657f54ba5ada423a
unknown <>
2017-05-15 07:08:45 +0900

    add video

--- tests/nav_ext_test.py
+++ tests/nav_ext_test.py
@@ -17,8 +17,8 @@ inter = Interaction(nav_env, bot)
 
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
 
-obs1 = nav_env.top_down_view(512)
-obs2 = nav_env.egocentric_view(512)
+obs1 = nav_env.top_down_view(128)
+obs2 = nav_env.egocentric_view(128)
 obs3 = nav_env.show_observation(inter._observation)
 
 im1 = ax1.imshow(obs1, origin='lower')
@@ -27,13 +27,13 @@ im3 = ax3.imshow(obs3, interpolation='none', origin='lower')
 
 def animate(*args):
     inter.interact()
-    im1.set_array(nav_env.top_down_view(512))
-    im2.set_array(nav_env.egocentric_view(512))
+    im1.set_array(nav_env.top_down_view(128))
+    im2.set_array(nav_env.egocentric_view(128))
     im3.set_array(nav_env.show_observation(inter._observation))
     return im1, im2, im3
 
 def run():
     ani = animation.FuncAnimation(fig, animate, 1000, interval=100, blit=True)
-    #ani.save("nav_env.mp4", writer='ffmpeg')
+    #ani.save("nav_env_ext.mp4", writer='ffmpeg')
     plt.show()
     return ani
